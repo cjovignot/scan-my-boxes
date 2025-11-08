4a54b6d57d9df732c0f8c1f0d0f4efe595581313
cjovignot <cosme.jovignot@epitech.eu>
2025-11-08 22:46:35 +0100

    Update Register.tsx

--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -2,7 +2,7 @@ import UserForm from "../components/UserForm";
 
 const Register = () => {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-950">
+    <div className="flex items-center justify-center bg-gray-950">
       <UserForm />
     </div>
   );
