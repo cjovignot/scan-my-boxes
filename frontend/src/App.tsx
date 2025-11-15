cc2f924712fe73750287519108bed06f257ac26c
cjovignot <cosme.jovignot@epitech.eu>
2025-11-15 14:49:19 +0100

    [FIX] Test container killed input PWA

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -147,7 +147,7 @@ function App() {
             <Route path="*" element={<Navigate to="/" replace />} />
           </Route>
         </Routes>
-        <FloatingPrintButton />
+        {/* <FloatingPrintButton /> */}
       </AnimatePresence>
     </AuthProvider>
   );
